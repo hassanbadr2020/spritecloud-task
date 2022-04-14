a1dbb8dd40a4d6b3da8152cd982d1f777128a568
hassanbadr2020 <eng,hassan54@yahoo.com>
2022-04-14 21:19:54 +0200

    add base classes to ui test module

---
+++ ui/src/test/java/base/Base.java
@@ -0,0 +1,30 @@
+package base;
+
+import environments.handler.DataManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Base {
+    public WebDriver driver;
+    DataManager dataManager = new DataManager();
+
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        Dimension initial_size = driver.manage().window().getSize();
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1024, 768));
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        driver.navigate().to(dataManager.getUIUrl());
+
+    }
+
+    public void tearBrowser() {
+        driver.quit();
+    }
+
+}
---
+++ ui/src/test/java/runners/Runner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        dryRun = false,
+        strict = true,
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:testOutput"},
+        features = {"src/test/resources/featureFiles"},
+        glue = {"stepDefinitions"}
+)
+
+public class Runner {
+}
