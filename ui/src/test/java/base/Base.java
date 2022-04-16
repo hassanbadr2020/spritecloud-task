76394cbf954b8f0d13e5e380b41eb183d9677409
hassanbadr2020 <eng,hassan54@yahoo.com>
2022-04-17 01:00:25 +0200

    add headless mode for chrome options

--- ui/src/test/java/base/Base.java
+++ ui/src/test/java/base/Base.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,7 +16,11 @@ public class Base {
 
     public void startBrowser() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
+
         Dimension initial_size = driver.manage().window().getSize();
         driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(1024, 768));
